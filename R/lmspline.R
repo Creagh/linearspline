f6103ec4d473626fced5c4f6dc421017b0d53092
Creagh Briercliffe <creagh5@hotmail.com>
2014-11-21 01:26:17 -0800

    edit to nknots name

--- R/lmspline.R
+++ R/lmspline.R
@@ -1,7 +1,9 @@
 #' Linear spline regression
 #'
 #' This function returns a list of coefficients and knots used
-#' to fit a linear spline to the given data.
+#' to fit a linear spline to the given data. Given a number of knots,
+#'  \code{nknots}, this function will place \code{nknots} knots at 
+#'  evenly spaced quantiles of the data, and fit a linear regression spline.
 #' 
 #' Linear spline regression works by fitting piece-wise linear 
 #' functions to segments of the data between connecting points, 
@@ -10,7 +12,7 @@
 #' 
 #' @param Vector \code{x} of the numeric valued data
 #' @param Vector \code{y} of the responses
-#' @param Numeric \code{num_knots} a positive integer corresponding
+#' @param Numeric \code{nknots} a positive integer corresponding
 #' to the number of knots
 #' @param Logical \code{na.rm} takes value \code{TRUE} if \code{NA} 
 #' values should be ignored
@@ -20,21 +22,21 @@
 #' @export
 #' @examples
 #' lmspline(x, y)
-#' lmspline(num_knots = 3, x, y, na.rm = FALSE)
+#' lmspline(nknots = 3, x, y, na.rm = FALSE)
 
-lmspline <- function(x, y, num_knots = 1, na.rm = FALSE) {
+lmspline <- function(x, y, nknots = 1, na.rm = FALSE) {
 	
-	# Check to see that num_knots is a numeric value
-	if(!is.numeric(num_knots)) {
+	# Check to see that nknots is a numeric value
+	if(!is.numeric(nknots)) {
 		stop('The number of knots must be a numeric value.')
 		
-		# Check to see that num_knots is a positive integer 
-	} else if(num_knots%%1 != 0 || num_knots <= 0) {
+		# Check to see that nknots is a positive integer 
+	} else if(nknots%%1 != 0 || nknots <= 0) {
 		stop('The number of knots must be a positive integer.')
 	}
 	
 	# Default: Choose the knots as evenly spaced quantiles of the data
-	knots <- x %>% quantile((1:num_knots) / (num_knots + 1), na.rm = na.rm)
+	knots <- x %>% quantile((1:nknots) / (nknots + 1), na.rm = na.rm)
 	
 	# Create the matrix of covariates to pass to the lm function
 	m <- create.cov(x, knots)
@@ -48,14 +50,14 @@ lmspline <- function(x, y, num_knots = 1, na.rm = FALSE) {
 
 # The create.cov subroutine creates a covariance matrix to be used in the linear regression
 create.cov <- function(x, knots) {
-	num_knots <- length(knots)
+	nknots <- length(knots)
 	
 	# Begin by initializing a matrix of zeroes with the apporpriate dimensions
-	m <- matrix(0, nrow = length(x), ncol = num_knots + 1)
+	m <- matrix(0, nrow = length(x), ncol = nknots + 1)
 	
 	# Fill the covariate matrix with the appropriate values based on the spline function
 	m[,1] <- x
-	for(i in 1:num_knots) {
+	for(i in 1:nknots) {
 		m[, i+1] <- pmax(0, x - knots[i])
 	}
 	return(m)
@@ -63,12 +65,12 @@ create.cov <- function(x, knots) {
 
 # The pred.spline function predicts values under the linear spline model
 pred.spline <- function(x, lspline) {
-	num_knots  <- length(lspline$knots)
+	nknots  <- length(lspline$knots)
 	fit <- lspline$fit
 	knots <- lspline$knots
 	
 	y = coef(fit)[1] + coef(fit)[2] * x
-	for(i in 1:num_knots) {
+	for(i in 1:nknots) {
 		y = y + coef(fit)[i+2] * pmax(0, x - knots[i])
 	}
 	return(y)
